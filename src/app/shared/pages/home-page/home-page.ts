99399163b6f9dcd5b88a0ca2639b2168e68f735a
EngelberAmaya <engelberamaya@gmail.com>
2026-04-14 16:00:07 -0400

    DaisyUI - Components

--- src/app/shared/pages/home-page/home-page.ts
+++ src/app/shared/pages/home-page/home-page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
 export class HomePage {
-
+  backgroundImage = 'https://a-static.besthdwallpaper.com/flags-life-countries-world-map-wallpaper-2560x1440-81698_51.jpg';
 }
